b7f727642a5a4b0860ca437036ace3c4743c77f5
LeeSeoHyeong1031 <champ36912@naver.com>
2024-11-30 18:55:50 +0900

    [STYLE] : TeleportSlash 발동속도 느림을 빠르게 수정하고 이펙트 Slash, 기모으기 제거

--- DarkShield/Assets/02.Scripts/Player/Player.cs
+++ DarkShield/Assets/02.Scripts/Player/Player.cs
@@ -2,86 +2,86 @@ using UnityEngine;
 
 public class Player : Unit
 {
-    public StateMachine playerStateMachine;
-    public Weapon curWeopon;
-    public PlayerMovement playerMovement;
-    public PlayerInputManager playerInputManager;
-    public PlayerHealth playerHealth;
-    public Animator playerAnimator;
+	public StateMachine playerStateMachine;
+	public Weapon curWeopon;
+	public PlayerMovement playerMovement;
+	public PlayerInputManager playerInputManager;
+	public PlayerHealth playerHealth;
+	public Animator playerAnimator;
 
-    [SerializeField] private Transform rightHand;
-    [SerializeField] private GameObject swordPrefab;
+	[SerializeField] private Transform rightHand;
+	[SerializeField] private GameObject swordPrefab;
 
-    private void Awake()
-    {
-        Init();
-        Instantiate(swordPrefab, rightHand);
-    }
+	private void Awake()
+	{
+		Init();
+		Instantiate(swordPrefab, rightHand);
+	}
 
-    private void Update()
-    {
-        if (playerStateMachine.CurState == playerStateMachine.walkState)
-        {
-            Move(new Vector3(playerInputManager.InputMoveDir.x, 0, playerInputManager.InputMoveDir.y).normalized);
-            playerMovement.Rotate(playerInputManager.InputMoveDir);
-        }
-        playerStateMachine.OnUpdate();
-    }
+	private void Update()
+	{
+		if (playerStateMachine.CurState == playerStateMachine.walkState)
+		{
+			Move(new Vector3(playerInputManager.InputMoveDir.x, 0, playerInputManager.InputMoveDir.y).normalized);
+			playerMovement.Rotate(playerInputManager.InputMoveDir);
+		}
+		playerStateMachine.OnUpdate();
+	}
 
-    private void Init()
-    {
-        playerStateMachine = new StateMachine(this);
-        //curWeopon = GetComponent<Weapon>();
-        playerMovement = GetComponent<PlayerMovement>();
-        playerInputManager = GetComponent<PlayerInputManager>();
-        playerHealth = GetComponent<PlayerHealth>();
-        playerAnimator = GetComponent<Animator>();
-        playerStateMachine.Init(playerStateMachine.idleState);
-        MoveSpeed = 5;
-    }
+	private void Init()
+	{
+		playerStateMachine = new StateMachine(this);
+		//curWeopon = GetComponent<Weapon>();
+		playerMovement = GetComponent<PlayerMovement>();
+		playerInputManager = GetComponent<PlayerInputManager>();
+		playerHealth = GetComponent<PlayerHealth>();
+		playerAnimator = GetComponent<Animator>();
+		playerStateMachine.Init(playerStateMachine.idleState);
+		MoveSpeed = 5;
+	}
 
-    public float moveDistance;
+	public float moveDistance;
 
-    public void CustomTeleport()
-    {
-        var renderers = GetComponentsInChildren<Renderer>();
-        foreach (var rend in renderers)
-        {
-            rend.enabled = false;
-        }
-        transform.position += transform.forward * moveDistance;
+	public void CustomTeleport()
+	{
+		//var renderers = GetComponentsInChildren<Renderer>();
+		//foreach (var rend in renderers)
+		//{
+		//    rend.enabled = false;
+		//}
+		transform.position += transform.forward * moveDistance;
 
-        foreach (var rend in renderers)
-        {
-            rend.enabled = true;
-        }
-    }
+		//foreach (var rend in renderers)
+		//{
+		//    rend.enabled = true;
+		//}
+	}
 
-    #region 애니메이션 이벤트 함수 ENDXX
-    public void EndDash()
-    {
-        playerStateMachine.dashState.EndDash();
-    }
+	#region 애니메이션 이벤트 함수 ENDXX
+	public void EndDash()
+	{
+		playerStateMachine.dashState.EndDash();
+	}
 
-    public void EndSkill()
-    {
-        playerStateMachine.skillState.EndSkill();
-    }
+	public void EndSkill()
+	{
+		playerStateMachine.skillState.EndSkill();
+	}
 
-    public void EndHit()
-    {
-        playerStateMachine.hitState.EndHit();
-    }
+	public void EndHit()
+	{
+		playerStateMachine.hitState.EndHit();
+	}
 
-    #endregion
+	#endregion
 
-    public override void TakeDamage(float amount, bool isHit)
-    {
-        playerHealth.TakeDamage(amount, isHit);
-    }
+	public override void TakeDamage(float amount, bool isHit)
+	{
+		playerHealth.TakeDamage(amount, isHit);
+	}
 
-    public override void Move(Vector3 dir)
-    {
-        playerMovement.Move(dir * MoveSpeed);
-    }
+	public override void Move(Vector3 dir)
+	{
+		playerMovement.Move(dir * MoveSpeed);
+	}
 }
