4d11ffd57725fefe4f39f1bbfbafd3f4dc711225
yugd1211 <yugd1211@gmail.com>
2024-12-04 10:38:03 +0900

    [REFACTOR] : GameManager가 모든 Manager들의 Init을 동기적으로 처리

--- DarkShield/Assets/02.Scripts/Managers/GameManager.cs
+++ DarkShield/Assets/02.Scripts/Managers/GameManager.cs
@@ -18,19 +18,25 @@ public partial class GameManager : MonoBehaviour
 	public Player player;
 	public Gold gold = new Gold(0);
 
-	public void Init()
+	private void Init()
 	{
-		EnemyManager newManager = new GameObject("EnemyManager").AddComponent<EnemyManager>();
-		newManager.transform.SetParent(transform);
-		enemyManager = newManager;
+		EnemyManager newEnemyManager = new GameObject("EnemyManager").AddComponent<EnemyManager>();
+		newEnemyManager.transform.SetParent(transform);
+		enemyManager = newEnemyManager;
 		enemyManager.Init(enemySpawnDatas[0]);
-		new GameObject("DialogueManager").AddComponent<DialogueManager>().transform.SetParent(transform);
+		
+		DialogueManager newDialogueManager = new GameObject("DialogueManager").AddComponent<DialogueManager>();
+		newDialogueManager.transform.SetParent(transform);
+		dialogueManager = newDialogueManager;
+		
 		stageManager = FindObjectOfType<StageManager>();
 		
 		// Player도 GameManager에서 생성하게끔 할 예정
 		// Player Init에 얽혀있는 것들이 너무 많아서 상의후 결정해햐할듯
 		// 예를들면 Weapon의 Init을 Awake에서 하는 것
 		player = FindObjectOfType<Player>();
+		// player = Instantiate(playerPrefab).GetComponent<Player>();
+		player.Init();
 	}
 }
 
