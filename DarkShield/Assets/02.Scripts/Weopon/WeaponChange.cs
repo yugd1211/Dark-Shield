f20e73f082cb3ec99a024415367139110443914f
yugd1211 <yugd1211@gmail.com>
2024-12-05 15:26:00 +0900

    [REFACTOR] : 무기 선택을 닿으면 갖는 형태에서 IInteractable을 상속받아서 상호작용 키를 눌러야 무기 선택할 수 있게끔 변경

--- DarkShield/Assets/02.Scripts/Weopon/WeaponChange.cs
+++ DarkShield/Assets/02.Scripts/Weopon/WeaponChange.cs
@@ -1,10 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
 
-public abstract class WeaponChange : MonoBehaviour
+public class WeaponChange : MonoBehaviour, IInteractable
 {
     public AnimatorController animController;
     public GameObject weaponPrefab;
+    public float rotateSpeed;
+    public bool CanInteract() => !GameManager.Instance.player.isEquip;
+    
+    public void Interact(Interactor player)
+    {
+        player.GetComponent<Player>().ChangeWeapon(this);
+        Destroy(gameObject);
+    }
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 10) * Time.deltaTime * rotateSpeed);
+    }
 }
