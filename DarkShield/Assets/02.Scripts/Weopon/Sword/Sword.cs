a23848c2b98f0dfa6ccd7df27cf02f7b1bfde81f
l97038789 <l97038789@gmail.com>
2024-12-06 11:41:26 +0900

    [STYLE] : 좌클릭 기능 구현에 따른 코드 수정

--- DarkShield/Assets/02.Scripts/Weopon/Sword/Sword.cs
+++ DarkShield/Assets/02.Scripts/Weopon/Sword/Sword.cs
@@ -1,12 +1,12 @@
 public class Sword : Weapon
 {
-	public override void UseSkill(ActionType skillType)
-	{
-		if (skills.TryGetValue(skillType, out Skill skill))
-		{
-			if (skill.skillData.isSkillAimingAtMouse) player.mouseLookDir.LookAt();
-			skill.UseSkill();
-			player.playerAnimator.SetTrigger(skillType.ToString());
-		}
-	}
+    public override void UseSkill(ActionType skillType)
+    {
+        if (skills.TryGetValue(skillType, out Skill skill))
+        {
+            if (skill.isSkillAimingAtMouse) player.mouseLookDir.LookAt();
+            skill.UseSkill();
+            player.playerAnimator.SetTrigger(skillType.ToString());
+        }
+    }
 }
