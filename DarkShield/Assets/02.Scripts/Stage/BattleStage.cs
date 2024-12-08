d0269b4f2ec890806b37321e9ce113cded06975d
yugd1211 <yugd1211@gmail.com>
2024-12-09 00:38:25 +0900

    [FEAT] : BattleStage에서 몬스터 모두 처치 후 생기는 업그레이드 패널 상호작용시 다음 포탈 상호작용이 가능하게 변경

--- DarkShield/Assets/02.Scripts/Stage/BattleStage.cs
+++ DarkShield/Assets/02.Scripts/Stage/BattleStage.cs
@@ -1,8 +1,12 @@
+using System.Collections;
 using UnityEngine;
 public class BattleStage : Stage
 {
 	public EnemySpawnData enemySpawnData;
 	public Transform[] enemySpawnPoint;
+	
+	public GameObject upgradeObjectPrefab;
+	public GameObject upgradeObjectSpawnPoint;
 	
 	public override void Init(StageManager stageManager)
 	{
@@ -15,5 +19,20 @@ public class BattleStage : Stage
 		enemySpawnData = GameManager.Instance.enemySpawnDatas[GameManager.Instance.stageManager.currentStageIndex];
 		enemyManager.Init(enemySpawnData);
 		enemyManager.enemySpawner.StartSpawning(enemySpawnPoint);
+		isStageCleared = false;
+		StartCoroutine(BattleEndCoroutine());
+	}
+
+	private IEnumerator BattleEndCoroutine()
+	{
+		yield return new WaitUntil(() => GameManager.Instance.enemyManager.enemySpawner.isAllWavesCompleted);
+		GameObject go = Instantiate(upgradeObjectPrefab, upgradeObjectSpawnPoint.transform.position, Quaternion.identity);
+		yield return new WaitUntil(() => go == null);
+		BattleEnd();
+	}
+
+	private void BattleEnd()
+	{
+		isStageCleared = true;
 	}
 }
--- DarkShield/Assets/02.Scripts/Stage/Stage.cs
+++ DarkShield/Assets/02.Scripts/Stage/Stage.cs
@@ -9,12 +9,16 @@ public class Stage : MonoBehaviour
 	public List<Transform> portalPoints = new List<Transform>();
 	public GameObject portalPrefab;
 	
+	
+	protected bool isStageCleared;
+	
 	// Init을 위에서 할당하다보니 인스펙터에서 할당해야함
 	public Transform playerStartPos;
 	public virtual void Init(StageManager stageManager)
 	{
 		this.stageManager = stageManager;
 		player = stageManager.player;
+		isStageCleared = true;
 	}
 	public void GoToStage()
 	{
@@ -24,19 +28,16 @@ public class Stage : MonoBehaviour
 			player.playerMovement.Spawn(playerStartPos.position);
 	}
 
-	public void CreateNextPortal()
+	public virtual void CreateNextPortal()
 	{
 		if (portalPrefab == null)
 			return;
 		Portal newPortal = Instantiate(portalPrefab, portalPoints[0].position, Quaternion.identity).GetComponent<Portal>();
 		newPortal.transform.SetParent(transform);
 		newPortal.Init();
-		if (this is BattleStage)
-			newPortal.OnCanInteract += () => GameManager.Instance.enemyManager.enemySpawner.isAllWavesCompleted;
-		else
-			newPortal.OnCanInteract += () => true;
 		portals.Add(newPortal);
 		portalPoints.RemoveAt(0);
+		newPortal.OnCanInteract += () => isStageCleared;
 	}
 
 	public void MoveNextStage(Stage moveStage)
