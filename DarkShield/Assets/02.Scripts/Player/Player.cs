0b1bb1b50632ab9667f3b1855e09b7117ae0ddc0
LeeSeoHyeong1031 <champ36912@naver.com>
2024-12-11 22:57:57 +0900

    [STYLE] : 플레이어 애니메이션 부드럽게 전체적으로 수정. 빠르게 인식하도록 수정

--- DarkShield/Assets/02.Scripts/Player/Player.cs
+++ DarkShield/Assets/02.Scripts/Player/Player.cs
@@ -3,135 +3,146 @@ using UnityEngine;
 
 public class Player : Unit
 {
-    public StateMachine playerStateMachine;
-    public Weapon curWeopon;
-    public PlayerMovement playerMovement;
-    public PlayerInputManager playerInputManager;
-    public PlayerHealth playerHealth;
-    public Animator playerAnimator;
-    public MouseLook mouseLookDir;
-    public Dash playerDash;
-    public PlayerStat playerStat;
-    public AnimationEventEffects playerAnimationEventEffects;
+	public StateMachine playerStateMachine;
+	public Weapon curWeopon;
+	public PlayerMovement playerMovement;
+	public PlayerInputManager playerInputManager;
+	public PlayerHealth playerHealth;
+	public Animator playerAnimator;
+	public MouseLook mouseLookDir;
+	public Dash playerDash;
+	public PlayerStat playerStat;
+	public AnimationEventEffects playerAnimationEventEffects;
 
-    [SerializeField] private Transform rightHand;
+	[SerializeField] private Transform rightHand;
 
-    //무기 장착
-    public bool isEquiped;
+	//무기 장착
+	public bool isEquiped;
 
-    private void Update()
-    {
-        if (playerStateMachine.CurState == playerStateMachine.walkState)
-        {
-            Move(new Vector3(playerInputManager.InputMoveDir.x, 0, playerInputManager.InputMoveDir.y).normalized);
-            playerMovement.Rotate(playerInputManager.InputMoveDir);
-        }
-        else
-        {
-            playerAnimator.SetBool("Walk", false);
-        }
-        playerStateMachine.OnUpdate();
-    }
+	private void Update()
+	{
+		if (playerStateMachine.CurState == playerStateMachine.walkState)
+		{
+			Move(new Vector3(playerInputManager.InputMoveDir.x, 0, playerInputManager.InputMoveDir.y).normalized);
+			playerMovement.Rotate(playerInputManager.InputMoveDir);
+		}
+		else
+		{
+			playerAnimator.SetBool("Walk", false);
+		}
+		playerStateMachine.OnUpdate();
+	}
 
-    public void Init()
-    {
-        playerStateMachine = new StateMachine(this);
-        playerMovement = GetComponent<PlayerMovement>();
-        playerInputManager = GetComponent<PlayerInputManager>();
-        playerHealth = GetComponent<PlayerHealth>();
-        playerAnimator = GetComponent<Animator>();
-        mouseLookDir = GetComponent<MouseLook>();
-        playerDash = GetComponent<Dash>();
-        playerStat = GetComponent<PlayerStat>();
-        playerAnimationEventEffects = GetComponent<AnimationEventEffects>();
-        StartCoroutine(OnFirstStart());
-        MoveSpeed = 5;
-        isDamaged = true;
-    }
+	public void Test()
+	{
+		print(1);
+	}
 
-    public IEnumerator OnFirstStart()
-    {
-        playerInputManager.CanInput = false;
-        while (true)
-        {
-            AnimatorStateInfo stateInfo = playerAnimator.GetCurrentAnimatorStateInfo(0);
-            if (stateInfo.IsName("Standing Up") && stateInfo.normalizedTime <= 0.6f)
-            {
-                yield return null;
-            }
-            else
-            {
-                break;
-            }
-        }
-        playerStateMachine.Init(playerStateMachine.idleState);
-        playerInputManager.CanInput = true;
-    }
+	public void Init()
+	{
+		playerStateMachine = new StateMachine(this);
+		playerMovement = GetComponent<PlayerMovement>();
+		playerInputManager = GetComponent<PlayerInputManager>();
+		playerHealth = GetComponent<PlayerHealth>();
+		playerAnimator = GetComponent<Animator>();
+		mouseLookDir = GetComponent<MouseLook>();
+		playerDash = GetComponent<Dash>();
+		playerStat = GetComponent<PlayerStat>();
+		playerAnimationEventEffects = GetComponent<AnimationEventEffects>();
+		StartCoroutine(OnFirstStart());
+		MoveSpeed = 5;
+		isDamaged = true;
+	}
 
-    public void ChangeWeapon(WeaponChange weapon)
-    {
-        playerAnimator.runtimeAnimatorController = weapon.animController;
-        curWeopon = Instantiate(weapon.weaponPrefab, rightHand).GetComponent<Weapon>();
-        curWeopon.Init(this);
-        isEquiped = true;
-    }
+	public IEnumerator OnFirstStart()
+	{
+		playerInputManager.CanInput = false;
+		while (true)
+		{
+			AnimatorStateInfo stateInfo = playerAnimator.GetCurrentAnimatorStateInfo(0);
+			if (stateInfo.IsName("Standing Up") && stateInfo.normalizedTime <= 0.6f)
+			{
+				yield return null;
+			}
+			else
+			{
+				break;
+			}
+		}
+		playerStateMachine.Init(playerStateMachine.idleState);
+		StartCoroutine(WaitInput());
+	}
 
-    public void ChangeElement(ElementChange element)
-    {
-        curWeopon.ChangeElement(element);
-    }
+	private IEnumerator WaitInput()
+	{
+		yield return new WaitForSeconds(0.5f);
+		playerInputManager.CanInput = true;
+	}
 
-    public float moveDistance;
+	public void ChangeWeapon(WeaponChange weapon)
+	{
+		playerAnimator.runtimeAnimatorController = weapon.animController;
+		curWeopon = Instantiate(weapon.weaponPrefab, rightHand).GetComponent<Weapon>();
+		curWeopon.Init(this);
+		isEquiped = true;
+	}
 
-    public void CustomTeleport()
-    {
-        transform.position += transform.forward * moveDistance;
-    }
+	public void ChangeElement(ElementChange element)
+	{
+		curWeopon.ChangeElement(element);
+	}
 
-    #region 애니메이션 이벤트 함수 ENDXX
-    public void EndDash()
-    {
-        playerStateMachine.dashState.EndDash();
-    }
+	public float moveDistance;
 
-    public void EndSkill()
-    {
-        playerStateMachine.skillState.EndSkill();
-    }
+	public void CustomTeleport()
+	{
+		transform.position += transform.forward * moveDistance;
+	}
 
-    public void EndHit()
-    {
-        playerStateMachine.hitState.EndHit();
-    }
+	#region 애니메이션 이벤트 함수 ENDXX
+	public void EndDash()
+	{
+		playerStateMachine.dashState.EndDash();
+	}
 
-    #endregion
+	public void EndSkill()
+	{
+		playerStateMachine.skillState.EndSkill();
+	}
 
-    public bool isDamaged = true;
-    public override void TakeDamage(float amount, bool isHit)
-    {
-        if (!isDamaged)
-            return;
-        playerHealth.TakeDamage(amount, isHit);
-    }
+	public void EndHit()
+	{
+		playerStateMachine.hitState.EndHit();
+	}
 
-    public override void Move(Vector3 dir)
-    {
-        playerMovement.Move(dir * MoveSpeed);
-    }
+	#endregion
 
-    public void DashUpgrade()
-    {
-        playerDash.maxDashCount++;
-    }
+	public bool isDamaged = true;
+	public override void TakeDamage(float amount, bool isHit)
+	{
+		if (!isDamaged)
+			return;
+		playerHealth.TakeDamage(amount, isHit);
+	}
 
-    public void Die()
-    {
-        StartCoroutine(Restart());
-    }
+	public override void Move(Vector3 dir)
+	{
+		playerMovement.Move(dir * MoveSpeed);
+	}
 
-    private IEnumerator Restart()
-    {
-        yield return new WaitForSeconds(5f);
-        GameManager.Instance.Restart();
-    }
+	public void DashUpgrade()
+	{
+		playerDash.maxDashCount++;
+	}
+
+	public void Die()
+	{
+		StartCoroutine(Restart());
+	}
+
+	private IEnumerator Restart()
+	{
+		yield return new WaitForSeconds(5f);
+		GameManager.Instance.Restart();
+	}
 }
