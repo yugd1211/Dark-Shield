ce110da5e70e433c291790a6599773b55b92e587
yugd1211 <yugd1211@gmail.com>
2024-12-11 14:54:47 +0900

    [FEAT] : 에너미 피격시 damage popup 뜨는 기능 추가

---
+++ DarkShield/Assets/02.Scripts/DamagePopup.cs
@@ -0,0 +1,18 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class DamagePopup : MonoBehaviour
+{
+	public TextMeshPro textMesh;
+
+	public void SetText(string text)
+	{
+		textMesh.SetText(text);
+	}
+
+	private void Update()
+	{
+		transform.position += new Vector3(0, 0.5f) * Time.deltaTime;
+	}
+}
